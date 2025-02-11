85d340bce86c3acd844f330ee32b573b47feed23
M.F.R.Siam <mfrforhad1@gmail.com>
2025-02-11 07:10:53 +0600

    User Module Added

---
+++ backend/Api/Module/User/User.Domain/Events/UserCreated.cs
@@ -0,0 +1,7 @@
+﻿using TalentMesh.Framework.Core.Domain.Events;
+
+namespace TalentMesh.Module.User.Domain.Events;
+public sealed record UserCreated : DomainEvent
+{
+    public User? User { get; set; }
+}
---
+++ backend/Api/Module/User/User.Domain/Events/UserUpdated.cs
@@ -0,0 +1,7 @@
+﻿using TalentMesh.Framework.Core.Domain.Events;
+
+namespace TalentMesh.Module.User.Domain.Events;
+public sealed record UserUpdated : DomainEvent
+{
+    public User? User { get; set; }
+}
