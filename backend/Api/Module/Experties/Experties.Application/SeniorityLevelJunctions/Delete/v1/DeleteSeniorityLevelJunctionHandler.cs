d4ef05a7dc40992792126aee73ba77635537e7f6
rakibul24466 <77064885+rakibul24466@users.noreply.github.com>
2025-03-04 02:18:27 +0600

    feat(SeniorityLevelJunction): Application Layer Implemented

---
+++ backend/Api/Module/Experties/Experties.Application/SeniorityLevelJunctions/Delete/v1/DeleteSeniorityLevelJunctionHandler.cs
@@ -0,0 +1,22 @@
+﻿using TalentMesh.Framework.Core.Persistence;
+using TalentMesh.Module.Experties.Domain;
+using TalentMesh.Module.Experties.Domain.Exceptions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TalentMesh.Module.Experties.Application.SeniorityLevelJunctions.Delete.v1;
+public sealed class DeleteSeniorityLevelJunctionHandler(
+    ILogger<DeleteSeniorityLevelJunctionHandler> logger,
+    [FromKeyedServices("seniorityleveljunctions:seniorityleveljunction")] IRepository<Experties.Domain.SeniorityLevelJunction> repository)
+    : IRequestHandler<DeleteSeniorityLevelJunctionCommand>
+{
+    public async Task Handle(DeleteSeniorityLevelJunctionCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        var SeniorityLevelJunction = await repository.GetByIdAsync(request.Id, cancellationToken);
+        if (SeniorityLevelJunction == null || seniorityleveljunction.DeletedBy != Guid.Empty) throw new SeniorityLevelJunctionNotFoundException(request.Id);
+        await repository.DeleteAsync(seniorityleveljunction, cancellationToken);
+        logger.LogInformation("SeniorityLevelJunction with id : {SeniorityLevelId} deleted", SeniorityLevelJunction.Id);
+    }
+}
