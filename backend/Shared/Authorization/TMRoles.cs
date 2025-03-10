1750778c62701e73bbd75e19b2f0d92413ae5f20
nafiulhasanhamim <mdnafiulhasanhamim12345@gmail.com>
2025-03-10 16:13:07 +0600

    chore(roles): Insert three additional roles into Roles table

--- backend/Shared/Authorization/TMRoles.cs
+++ backend/Shared/Authorization/TMRoles.cs
@@ -6,11 +6,17 @@ public static class TMRoles
 {
     public const string Admin = nameof(Admin);
     public const string Basic = nameof(Basic);
+    public const string Candidate = nameof(Candidate);
+    public const string HR = nameof(HR);
+    public const string Interviewer = nameof(Interviewer);
 
     public static IReadOnlyList<string> DefaultRoles { get; } = new ReadOnlyCollection<string>(new[]
     {
         Admin,
-        Basic
+        Basic,
+        Candidate,
+        HR,
+        Interviewer
     });
 
     public static bool IsDefault(string roleName) => DefaultRoles.Any(r => r == roleName);
