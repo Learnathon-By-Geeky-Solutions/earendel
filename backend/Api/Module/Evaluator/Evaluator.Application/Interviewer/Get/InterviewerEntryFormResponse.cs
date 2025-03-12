6496b95c30d712c00249208b8a9638385e5cd72e
M.F.R.Siam <mfrforhad1@gmail.com>
2025-03-12 20:17:32 +0600

    feat(Evaluator): Get Implemented for this module

---
+++ backend/Api/Module/Evaluator/Evaluator.Application/Interviewer/Get/InterviewerEntryFormResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TalentMesh.Module.Evaluator.Application.Interviewer.Get.v1
+{
+    public sealed record InterviewerEntryFormResponse(
+        Guid? Id,
+        Guid UserId,
+        string? AdditionalInfo,
+        string Status
+    );
+}
