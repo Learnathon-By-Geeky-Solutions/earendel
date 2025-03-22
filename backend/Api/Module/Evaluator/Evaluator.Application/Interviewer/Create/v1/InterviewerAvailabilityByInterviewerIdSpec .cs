7cd8b3bad4b9d8184ec38d94e73527809324a47b
nafiulhasanhamim <mdnafiulhasanhamim12345@gmail.com>
2025-03-22 15:37:04 +0600

    refactor: Enhance interviewer availability command and specs

---
+++ backend/Api/Module/Evaluator/Evaluator.Application/Interviewer/Create/v1/InterviewerAvailabilityByInterviewerIdSpec .cs
@@ -0,0 +1,15 @@
+using Ardalis.Specification;
+using TalentMesh.Module.Evaluator.Domain;
+using System;
+
+namespace TalentMesh.Module.Evaluator.Application.Interviewer.Create.v1
+{
+    public class InterviewerAvailabilityByInterviewerIdSpec : Specification<InterviewerAvailability>
+    {
+        public InterviewerAvailabilityByInterviewerIdSpec(Guid interviewerId)
+        {
+            Query.Where(availability => availability.InterviewerId == interviewerId);
+        }
+    }
+}
+
