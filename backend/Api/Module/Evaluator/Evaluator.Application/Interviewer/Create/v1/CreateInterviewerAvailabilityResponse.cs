b65f05569889c53e8cef391a285df772e277915b
rakibul24466 <77064885+rakibul24466@users.noreply.github.com>
2025-03-22 17:19:04 +0600

    fix: github build error fixed due to wrong namespace

--- backend/Api/Module/Evaluator/Evaluator.Application/Interviewer/Create/v1/CreateInterviewerAvailabilityResponse.cs
+++ backend/Api/Module/Evaluator/Evaluator.Application/Interviewer/Create/v1/CreateInterviewerAvailabilityResponse.cs
@@ -1,4 +1,4 @@
-﻿using namespace TalentMesh.Module.Evaluator.Application.Interviewer.Create.v1;
+﻿namespace TalentMesh.Module.Evaluator.Application.Interviewer.Create.v1;
 
 public sealed record CreateInterviewerAvailabilityResponse(
     List<Guid> AvailabilityIds // Updated from single Guid? to List<Guid>
