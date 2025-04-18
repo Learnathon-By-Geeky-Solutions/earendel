1f3297d1514b112ba20c8c06c18630665c3b6e76
M.F.R.Siam <mfrforhad1@gmail.com>
2025-04-18 09:17:42 +0600

    feat(Interviewer): Request Interviewer Command Added

---
+++ backend/Api/Module/InterviewerView/InterviewerView/RequestInterviewerCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace TalentMesh.Module.Evaluator.Application.Interviewer
+{
+    public record RequestInterviewerCommand(Guid UserId, string? AdditionalInfo) : IRequest<Guid>;
+}
