ce2a9d0cdf9e71aa746581b8ba913213a6cc8fab
M.F.R.Siam <mfrforhad1@gmail.com>
2025-04-18 09:18:29 +0600

    feat(Interviewer): Added REQ Interviewer Handler

---
+++ backend/Api/Module/InterviewerView/InterviewerView/RequestInterviewerHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using TalentMesh.Framework.Core.Domain.Contracts;
+using TalentMesh.Framework.Core.Persistence;
+using TalentMesh.Module.Evaluator.Domain;
+
+namespace TalentMesh.Module.Evaluator.Application.Interviewer
+{
+    public class RequestInterviewerHandler : IRequestHandler<RequestInterviewerCommand, Guid>
+    {
+        private readonly IRepository<InterviewerEntryForm> _repo;
+        public RequestInterviewerHandler(IRepository<InterviewerEntryForm> repo) => _repo = repo;
+
+        public async Task<Guid> Handle(RequestInterviewerCommand req, CancellationToken ct)
+        {
+            var form = InterviewerEntryForm.Create(req.UserId, req.AdditionalInfo);
+            await _repo.AddAsync(form, ct);
+            return form.Id;
+        }
+    }
+}
